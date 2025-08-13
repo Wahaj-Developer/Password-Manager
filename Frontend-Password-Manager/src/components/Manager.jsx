71c40d4a61b0fd8200670bca65c7a5b0c197e07f
Wahaj Khan <ahmedwahaj317@gmail.com>
2025-08-13 23:42:47 +0500

    Update Manager.jsx

--- Frontend-Password-Manager/src/components/Manager.jsx
+++ Frontend-Password-Manager/src/components/Manager.jsx
@@ -72,3 +72,46 @@ if(confirmDelete) {
 
 
   }
+
+    
+  const editPassword = (id) => {
+
+     setform(passwordArray.filter(i=>i.id===id)[0]);
+     setpasswordArray(passwordArray.filter(item => item.id!== id));
+    
+
+
+  }
+
+  const handelChange = (e) => {
+
+    setform({ ...form, [e.target.name]: e.target.value })
+
+  }
+
+
+
+  return (
+    <>
+    <ToastContainer
+  position="top-right"
+  autoClose={5000}
+  hideProgressBar={false}
+  newestOnTop={false}
+  closeOnClick={false}  
+  rtl={false}
+  pauseOnFocusLoss
+  draggable
+  pauseOnHover
+  theme="dark"
+  transition="Bounce"
+/>
+
+
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div></div><div className="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-400 opacity-20 blur-[100px]"></div></div>
+       <div className=" p-2 pt-5 md:mycontainer min-h-[88.2vh] ">
+        <h1 className='text-white text-4xl font-bold text-center'>
+          <span className="text-green-700">&lt;</span>
+          <span>Pass</span><span className="text-green-700">Op&gt;</span>
+        </h1>
+        <p className='text-center text-lg text-green-900'>Your own password manager</p>
