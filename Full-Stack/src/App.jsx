83da9769889feeff0ffad047f20cc4c43aa893a7
Wahaj Khan <ahmedwahaj317@gmail.com>
2025-08-19 23:23:50 +0500

    Add files via upload

---
+++ Full-Stack/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Manager from './components/Manager';
+import Footer from './components/Footer';
+function App() {
+  
+
+  return (
+    <>
+    
+      <Navbar />
+   <div className=" bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+
+    
+   </div>
+ <Manager />
+     
+    
+      <Footer />
+   
+    </>
+  )
+}
+
+export default App
