d57776f04f105426d24820d6862e84259fff021a
Wahaj Khan <ahmedwahaj317@gmail.com>
2025-08-24 00:02:18 +0500

    Update Manager.jsx

--- Full-Stack/src/components/Manager.jsx
+++ Full-Stack/src/components/Manager.jsx
@@ -141,4 +141,37 @@ if(confirmDelete) {
   transition="Bounce"
 />
 
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div></div><div className="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-400 opacity-20 blur-[100px]"></div></div>
+       <div className=" p-2 pt-5 md:mycontainer min-h-[88.2vh] ">
+        <h1 className='text-white text-4xl font-bold text-center'>
+          <span className="text-green-700">&lt;</span>
+          <span>Pass</span><span className="text-green-700">Op&gt;</span>
+        </h1>
+        <p className='text-center text-lg text-green-900'>Your own password manager</p>
+
+
+         <div className="flex flex-col  w-full justify-between gap-8">
+          <input value={form.site} onChange={handelChange} placeholder='Enter website URL' className='w-full rounded-full p-4 py-1 bg-white border border-green-500 outline-none' type="text" name='site' id='site' />
+          <div className="flex w-full  justify-between gap-4">
+            <input value={form.username} onChange={handelChange} placeholder='Enter user name' className=' w-full rounded-full p-4 py-1 bg-white border border-green-500 outline-none' type="text" name='username' id='username' />
+            <div className="relative items-center">
+
+
+              <input ref={passwordRef} value={form.password} onChange={handelChange} placeholder='Enter password' className=' w-full rounded-full p-4 py-1 bg-white border border-green-500 outline-none' type="password" name='password' id='password' />
+
+              <span className="absolute right-[3px] top-[4px] cursor-pointer items-center" onClick={showPassword}>
+                <img ref={ref} className='p-1' width={25} src="/eye.png" alt="" />
+              </span>
+            </div>
+
+          </div>
+          <button onClick={savePassword} className='flex justify-center items-center text-green-700 border-2 hover:border-none px-4 border-green-700 hover:bg-green-700 hover:text-white rounded-full w-fit p-2 '>
+            <lord-icon 
+              src="https://cdn.lordicon.com/sbnjyzil.json"
+              trigger="hover"
+               stroke="bold">
+            </lord-icon>
+            Add Password</button>
+        </div>
+
 
